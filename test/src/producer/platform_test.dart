9297ffa6820459cfa9e1062c22d9cd643c79edcd
buijs <jjgbuijs@gmail.com>
2022-06-03 08:43:59 +0200

    Add tests for klutter module creation.

--- test/src/producer/platform_test.dart
+++ test/src/producer/platform_test.dart
@@ -21,6 +21,7 @@
 import "dart:io";
 
 import "package:klutter/src/common/exception.dart";
+import "package:klutter/src/common/shared.dart";
 import "package:klutter/src/producer/platform.dart";
 import "package:test/test.dart";
 
@@ -325,6 +326,248 @@ void main() {
        kotlin.mpp.enableGranularSourceSetsMetadata=true
        kotlin.native.enableDependencyPropagation=false
        kotlin.mpp.enableCInteropCommonization=true""".replaceAll(" ", ""));
+
+    root.deleteSync(recursive: true);
+
+  });
+
+  test("Verify a  root/klutter module is created", () {
+
+    final root = Directory("${Directory.systemTemp.path}${s}wsg7")
+      ..createSync(recursive: true);
+
+    createKlutterModule(
+        pathToRoot: root.path,
+        pluginName: "nigulp",
+        packageName: "com.organisation.nigulp",
+    );
+
+    final android =  Directory("${root.path}/klutter/android".normalize);
+
+    expect(true, android.existsSync(),
+      reason: "root/klutter/android should be created",
+    );
+
+    final androidBuildGradle = File("${android.path}/build.gradle.kts".normalize);
+
+    expect(true, androidBuildGradle.existsSync(),
+      reason: "root/klutter/android/build.gradle.kts should be created",
+    );
+
+    expect("""
+      configurations.maybeCreate("default")
+      artifacts.add("default", file("nigulp.aar"))
+      """.replaceAll(" ", ""),
+      androidBuildGradle.readAsStringSync().replaceAll(" ", ""),
+    );
+
+    final platform =  Directory("${root.path}/klutter/nigulp".normalize);
+
+    expect(true, android.existsSync(),
+      reason: "root/klutter/nigulp should be created",
+    );
+
+    final platformBuildGradle = File("${platform.path}/build.gradle.kts".normalize);
+
+    expect(true, platformBuildGradle.existsSync(),
+      reason: "root/klutter/nigulp/build.gradle.kts should be created",
+    );
+
+    expect("""
+     plugins {
+          id("com.android.library")
+          id("dev.buijs.klutter.gradle")
+          kotlin("multiplatform")
+          kotlin("native.cocoapods")
+          kotlin("plugin.serialization") version "1.6.10"
+      }
+      
+      version = "1.0"
+      
+      klutter {
+          root = rootProject.rootDir
+          plugin { 
+             name = "nigulp"
+          }
+      }
+      
+      kotlin {
+          android()
+          iosX64()
+          iosArm64()
+      
+          cocoapods {
+              summary = "Some description for the Shared Module"
+              homepage = "Link to the Shared Module homepage"
+              ios.deploymentTarget = "14.1"
+              framework {
+                  baseName = "TODO"
+              }
+          }
+          
+          sourceSets {
+      
+              val commonMain by getting {
+                  dependencies {
+                      api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+                      api("dev.buijs.klutter:annotations-kmp:2022-alpha-3")
+                  }
+              }
+      
+              val commonTest by getting {
+                  dependencies {
+                      implementation(kotlin("test-common"))
+                      implementation(kotlin("test-annotations-common"))
+                      implementation(kotlin("test-junit"))
+                      implementation("junit:junit:4.13.2")
+                      implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+                  }
+              }
+      
+              val androidMain by getting {
+                  dependencies {
+                      implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+                  }
+              }
+      
+              val androidTest by getting {
+                  dependencies {
+                      implementation(kotlin("test-junit"))
+                      implementation("junit:junit:4.13.2")
+                  }
+              }
+      
+              val iosX64Main by getting
+              val iosArm64Main by getting
+              val iosMain by creating {
+                  dependsOn(commonMain)
+                  iosX64Main.dependsOn(this)
+                  iosArm64Main.dependsOn(this)
+                  dependencies {}
+              }
+      
+              val iosX64Test by getting
+              val iosArm64Test by getting
+              val iosTest by creating {
+                  dependsOn(commonTest)
+                  iosX64Test.dependsOn(this)
+                  iosArm64Test.dependsOn(this)
+              }
+          }
+      }
+      
+      android {
+          compileSdk = 31
+          sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+          defaultConfig {
+              minSdk = 21
+              targetSdk = 31
+          }
+      }""".replaceAll(" ", ""),
+      platformBuildGradle.readAsStringSync().replaceAll(" ", ""),
+    );
+
+    final androidMain =  Directory("${platform.path}/src/androidMain/kotlin/com/organisation/nigulp/platform".normalize);
+
+    expect(true, androidMain.existsSync(),
+      reason: "root/klutter/nigulp/src/androidMain/kotlin/com/organisation/nigulp/platform should be created",
+    );
+
+    final commonMain =  Directory("${platform.path}/src/commonMain/kotlin/com/organisation/nigulp/platform".normalize);
+
+    expect(true, commonMain.existsSync(),
+      reason: "root/klutter/nigulp/src/commonMain/kotlin/com/organisation/nigulp/platform should be created",
+    );
+
+    final iosMain =  Directory("${platform.path}/src/iosMain/kotlin/com/organisation/nigulp/platform".normalize);
+
+    expect(true, iosMain.existsSync(),
+      reason: "root/klutter/nigulp/src/iosMain/kotlin/com/organisation/nigulp/platform should be created",
+    );
+
+    final androidManifest =  File("${platform.path}/src/androidMain/AndroidManifest.xml".normalize);
+
+    expect(true, androidManifest.existsSync(),
+      reason: "root/klutter/nigulp/src/androidMain/AndroidManifest.xml should be created",
+    );
+
+    expect( """
+        <?xml version="1.0" encoding="utf-8"?>
+        <manifest package="com.organisation.nigulp.platform" />
+        """.replaceAll(" ", ""),
+      androidManifest.readAsStringSync().replaceAll(" ", ""),
+    );
+
+    final androidPlatformClass =  File("${androidMain.path}/Platform.kt".normalize);
+
+    expect(true, androidPlatformClass.existsSync(),
+      reason: "root/klutter/nigulp/src/androidMain/kotlin/com/organisation/nigulp/platform/Platform.kt should be created",
+    );
+
+    expect(r"""
+      package com.organisation.nigulp.platform
+      
+      actual class Platform actual constructor() {
+          actual val platform: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+      }
+      """.replaceAll(" ", ""),
+      androidPlatformClass.readAsStringSync().replaceAll(" ", ""),
+    );
+
+    final commonGreetingClass =  File("${commonMain.path}/Greeting.kt".normalize);
+
+    expect(true, commonGreetingClass.existsSync(),
+      reason: "root/klutter/nigulp/src/commonMain/kotlin/com/organisation/nigulp/platform/Greeting.kt should be created",
+    );
+
+    expect(r"""
+      package com.organisation.nigulp.platform
+      
+      import dev.buijs.klutter.annotations.kmp.KlutterAdaptee
+      
+      class Greeting {
+      
+          @KlutterAdaptee(name = "greeting")
+          fun greeting(): String {
+             return "Hello, ${Platform().platform}!"
+          }
+      
+      }""".replaceAll(" ", ""),
+      commonGreetingClass.readAsStringSync().replaceAll(" ", ""),
+    );
+
+    final commonPlatformClass =  File("${commonMain.path}/Platform.kt".normalize);
+
+    expect(true, commonPlatformClass.existsSync(),
+      reason: "root/klutter/nigulp/src/commonMain/kotlin/com/organisation/nigulp/platform/Platform.kt should be created",
+    );
+
+    expect("""
+      package com.organisation.nigulp.platform
+      
+      expect class Platform() {
+          val platform: String
+      }""".replaceAll(" ", ""),
+      commonPlatformClass.readAsStringSync().replaceAll(" ", ""),
+    );
+
+    final iosPlatformClass =  File("${iosMain.path}/Platform.kt".normalize);
+
+    expect(true, iosPlatformClass.existsSync(),
+      reason: "root/klutter/nigulp/src/iosMain/kotlin/com/organisation/nigulp/platform/Platform.kt should be created",
+    );
+
+    expect("""
+      package com.organisation.nigulp.platform
+      
+      import platform.UIKit.UIDevice
+      
+      actual class Platform actual constructor() {
+          actual val platform: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+      }
+      """.replaceAll(" ", ""),
+      iosPlatformClass.readAsStringSync().replaceAll(" ", ""),
+    );
 
     root.deleteSync(recursive: true);
 
